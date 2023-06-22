de833138cf3a7be444e3bcf43acef2d773dbb0fc
Weimin Wan <weiminwan@Weimins-MBP.localdomain>
2023-06-22 10:16:01 -0400

    finish tutorial, everything works fine; add style

--- index.js
+++ index.js
@@ -18,6 +18,37 @@ app.get('/', (req, res) => { res.sendFile(__dirname + '/views/index.html') })
 app.get('/student', (req, res) => { res.sendFile(__dirname + '/views/student.html') })
 app.get('/lecturer', (req, res) => { res.sendFile(__dirname + '/views/lecturer.html') })
 
+app.post('/auth', async (req, res) => {
+    try {
+      const { id, key } = req.body
+      if(req.body.key != process.env.LECTURE_KEY) {
+        return res.json({ error: 'Lecture Key is missing or incorrect' })
+      }
+
+      const newKey = await deepgram.keys.create(
+          process.env.DEEPGRAM_PROJECT,
+          'Temporary key - works for 10 secs',
+          [ 'usage:write' ],
+          { timeToLive: 10 }
+      )
+      res.json({ deepgramToken: newKey.key })
+    } catch(error) {
+      res.json({ error })
+    }
+  })
+  
+  io.on('connection', socket => {
+    socket.on('join', roomId => {
+        socket.join(roomId)
+        console.log(`${socket.id} joins ${roomId}`)
+    })
+    socket.on('transcriptReady', message => {
+      for (let room of socket.rooms) {
+        socket.to(room).emit('transcriptComplete', message)
+      }
+    })
+  })
+
 // Start server
 server.listen(PORT, () => {
     console.log(`Listening on ${PORT}...`);
